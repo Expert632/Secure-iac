b2e1c2037b02b9371ce085b5bc0d73ade99bbd94
Expert632 <machane.salim@gmail.com>
2025-10-25 20:58:20 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,11 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
   required_version = ">= 1.5.0"
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
+# Provider "local" = 100% local, zéro cloud
+provider "local" {}
 
-# Dummy Resource (Testing Checkov Security Scan)
-resource "aws_s3_bucket" "my_insecure_bucket" {
-  bucket = "insecure-example-bucket-1234"
-
-  # ❌ Public Access (Checkov va détecter une vulnérabilité)
-  acl    = "public-read"
+resource "local_file" "example" {
+  filename = "${path.module}/hello.txt"
+  content  = "Hello GitHub Actions Lab!"
 }
